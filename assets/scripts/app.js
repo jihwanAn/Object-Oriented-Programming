c6f1c208e17f5424725a63ce06d56d4134fccd82
jihwanAn <an0572@naver.com>
2023-01-19 22:11:07 +0900

    정적 메소드 & 프로퍼티

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,6 +16,11 @@ class Product {
 class ShoppingCart {
   item = [];
 
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: ${1}</h2>`;
+  }
+
   render() {
     const cartEl = document.createElement("section");
     cartEl.innerHTML = `
@@ -23,6 +28,7 @@ class ShoppingCart {
       <buuton>Order Now!</button>
     `;
     cartEl.classList = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
   }
 }
@@ -33,8 +39,7 @@ class ProductItem {
   }
 
   addToCart() {
-    console.log("Adding product to cart..");
-    console.log(this.product);
+    App.addProductToCart(this.product);
   }
 
   render() {
@@ -90,8 +95,8 @@ class Shop {
   render() {
     const renderHook = document.getElementById("app");
 
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
 
@@ -100,5 +105,18 @@ class Shop {
   }
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
